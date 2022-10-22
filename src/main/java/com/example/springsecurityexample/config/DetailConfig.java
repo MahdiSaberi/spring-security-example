278b04976752b1f8ead51138cf4849f1297c48f5
mahdisaberi <mahdisaberi35@gmail.com>
2022-10-22 12:03:36 +0330

    using different way for configure

--- src/main/java/com/example/springsecurityexample/config/DetailConfig.java
+++ src/main/java/com/example/springsecurityexample/config/DetailConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecurityexample.config;
 import net.bytebuddy.asm.MemberSubstitution;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,22 +19,34 @@ import java.nio.charset.StandardCharsets;
 public class DetailConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("admin").password("123").authorities("read").build();
+
+        userDetailsManager.createUser(userDetails);
+
+        auth.userDetailsService(userDetailsManager).passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails userDetails = User.withUsername("admin").password("123").authorities("read").build();
-        userDetailsManager.createUser(userDetails);
-        return userDetailsManager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+//        UserDetails userDetails = User.withUsername("admin").password("123").authorities("read").build();
+//        userDetailsManager.createUser(userDetails);
+//        return userDetailsManager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 }
